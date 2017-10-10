000ea9d59bc8e6ddfe309358e932cfc08b8b98ba
jlaue <justin@fp-x.com>
2017-10-10 09:26:55 -0600

    remove bs58check, add ts def for sha256sha256

--- index.d.ts
+++ index.d.ts
@@ -13,22 +13,15 @@
 // export function decrypt(cipher: string, privateKey: string, algorithm?: string): any; // change to Promise?
 // export function encrypt(plaintext: string, publicKey: string, algorithm?: string): any; // change to Promise?
 
-// declare class bs58check {
-// 	encode(buf: Buffer): string;
-// 	decode(str58: string): Buffer;
-// }
-
-// export let bs58check: bs58check;
-
 declare class ecc {
-	static bs58check: any;
 	public static privateKey(): any;
-	public static publicKey(privateKey: Buffer, useCompressedKeys: boolean): Buffer;
+	public static publicKey(privateKey: Buffer, useCompressedKeys?: boolean): Buffer;
 	public static decrypt(cipher: string, privateKey: string, algorithm?: string): any; // change to Promise?
 	public static encrypt(plaintext: string, publicKey: string, algorithm?: string): any; // change to Promise?
-	public static sha256ripemd160(msg: Buffer): Buffer; // change to Promise?
+	public static sha256ripemd160(msg: Buffer): Buffer; 
+	public static sha256sha256(msg: Buffer): Buffer; 
 
 }
 
 export = ecc;
-export as namespace ecc;
+// export as namespace ecc;
