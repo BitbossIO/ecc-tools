633969524e94c80c6f1ca677313c4a62899b0872
Alex Shore <ford152@yahoo.com>
2017-10-31 17:59:20 -0600

    added type defs for cipher, decipher

--- index.d.ts
+++ index.d.ts
@@ -18,8 +18,10 @@ declare class ecc {
 	public static publicKey(privateKey: Buffer, useCompressedKeys?: boolean): Buffer;
 	public static decrypt(cipher: string, privateKey: string, algorithm?: string): any; // change to Promise?
 	public static encrypt(plaintext: string, publicKey: string, algorithm?: string): any; // change to Promise?
-	public static sha256ripemd160(msg: Buffer): Buffer; 
-	public static sha256sha256(msg: Buffer): Buffer; 
+	public static sha256ripemd160(msg: Buffer): Buffer;
+	public static sha256sha256(msg: Buffer): Buffer;
+	public static cipher(plaintext: string, key: Buffer, iv: string, algorithm?: string): any;
+	public static decipher(ciphertext: string, key: Buffer, iv: string, algorithm?: string): any;
 
 }
 
